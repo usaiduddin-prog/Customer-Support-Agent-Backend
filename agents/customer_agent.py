0b39166b4ea8012d197b82c817181b41efac94f3
usaiduddin-prog <usaid.uddin@daffodilsw.com>
2026-02-09 12:48:36 +0530

    fix import

--- agents/customer_agent.py
+++ agents/customer_agent.py
@@ -1,4 +1,4 @@
-from langchain.agents import AgentExecutor
+from langchain.agents.agent import AgentExecutor
 from langchain.agents.tool_calling_agent import create_tool_calling_agent
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
